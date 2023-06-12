559522de75783581d9dfdf9c9f5d96c1f6932360
franciscojrdev <franciscojunior7222@gmail.com>
2023-06-11 22:11:42 -0300

    feat: typescript initial config

---
+++ src/routes/events-router.ts
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const eventController = require("../controllers/events-controller");
+
+const eventsRouter = Router();
+
+eventsRouter.get("/", eventController.getEvents);
+eventsRouter.get("/:content", eventController.getEventsBySearch);
+eventsRouter.post("/", eventController.createEvent);
+eventsRouter.delete("/:id", eventController.deleteEvent);
+eventsRouter.put("/:id",eventController.updateEvent)
+
+module.exports = eventsRouter;
